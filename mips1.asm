907c182721fd28a818e6b00e69de5f8f526af31a
leenaabdalrahman <leenasa272@hmail.com>
2024-11-21 23:56:20 +0200

    first commit

--- mips1.asm
+++ mips1.asm
@@ -9,7 +9,10 @@ newline: .asciiz "\n"    # ت
 Arrays: .space 512          # لتخزين عدة معادلات (4 معادلات × 128 بايت لكل معادل
 Coefficient: .space 512
 Variables: .space 128
-Results: .space 128
+Results: .space 512
+LineCount: .word 0
+result1: .float 0.0  # لتخزين نتيجة القسمة الأولى
+result2: .float 0.0  # لتخزين نتيجة القسمة الثانية
 .text
 .globl main
 main:
@@ -20,7 +23,6 @@ main:
     syscall
     bltz $v0, error      # إذا فشل فتح الملف، انتقل إلى error
     move $s0, $v0        # حفظ معرف الملف
-
     # قراءة البيانات من الملف
     li $v0, 14           # syscall للقراءة
     move $a0, $s0        # معرف الملف
@@ -28,7 +30,6 @@ main:
     li $a2, 1024         # حجم البيانات المراد قراءتها
     syscall
     bltz $v0, error      # إذا فشلت القراءة، انتقل إلى error
-
     # تحليل البيانات المقروءة
     la $t0, fileWords    # مؤشر إلى بداية البيانات
     la $t1, lineBuffer   # مؤشر لتخزين معادلة واحدة
@@ -38,6 +39,9 @@ main:
 
 read_line:
     lb $t2, 0($t0)       # قراءة بايت واحد من buffer
+    #lw $t6,LineCount
+    #li $s2,3
+    #beq $t6,$s2,Hello
     beqz $t2, Check      # إذا وصلنا إلى نهاية البيانات، انتقل إلى إغلاق الملف
     li $t3, 10           # تحميل القيمة ASCII للسطر الجديد ('\n') في $t3
     beq $t2, $t3, process_line # إذا كان الحرف سطرًا جديدًا، انتقل لمعالجة المعادلة
@@ -45,28 +49,34 @@ read_line:
     addi $t0, $t0, 1     # الانتقال إلى الحرف التالي في buffer
     addi $t1, $t1, 1     # الانتقال إلى الموقع التالي في lineBuffer
     j read_line          # كرر قراءة الحرف التالي
+    
 Check:
     move $s6,$s7
     move $t1, $s7         # تحميل العنوان في $t1
     lb $t3, 0($t1)        # قراءة أول قيمة من العنوان
+    #li $t9, '-'
+   # beq $t3, $t9, negative # إذا كان الحرف '-'
     la $t0,Coefficient
     subi $t0,$t0,2
     la $t2,Variables
     la $t7,Results
+    subi $t7,$t7,2
     move $t5,$t7
     move $t8,$t2
+    
 check_number:
     li $s2, 48            # ASCII للصفر
     li $s3, 57            # ASCII للتسعة
     li $s4, 65              # ASCII للحرف 'A'
     li $s5, 90              # ASCII للحرف 'Z'
-    li $t5,0
-     
+    li $t5,0 
 check_loop:
     beqz $t3,go_Next  # إذا كانت القيمة صفرًا (نهاية النص)، إنهاء
     li $t6,61
     beq $t6,$t3,go_Next
 Coefi_:
+    li $t9, '-'
+    beq $t3, $t9, negative # إذا كان الحرف '-'
     blt $t3, $s2, next_char # إذا كانت أقل من ASCII للصفر، انتقل إلى الخانة التالية
     bgt $t3, $s3, next_char # إذا كانت أكبر من ASCII للتسعة، انتقل إلى الخانة التالية  لي 
     subi $t3,$t3,48
@@ -82,44 +92,63 @@ Coefi_:
     syscall
     # الانتقال إلى العنوان التالي
     j next_char 
-   
+negative:
+    move $s0,$t3
+    addi $t1, $t1, 1      # الانتقال إلى العنوان التالي
+    lb $t3, 0($t1)        # قراءة البايت التالي
+    j Coefi_
 go_Next:
     beq $t3, $t6, ADD_Results  # إذا كانت القيمة '=', انتقل إلى ADD_Results
     addi $s7, $s7, 32          # تحريك $s7
     lb $t3, 0($s7)             # قراءة القيمة التالية
     move $t1, $s7              # تحديث $t1
     addi $t0, $t0, 24     # تحديث مؤشر Coefficient
-    addi $t7,$t7,30
-    beqz $t3, end_check        # إذا كانت القيمة صفرًا، إنهاء
+    addi $t7,$t7,28
+    lw $t6,LineCount     ################################################################
+    li $s2,3
+    beq $t6,$s2,CR3      ###############################################################
+    beqz $t3, CR       # إذا كانت القيمة صفرًا، إنهاء
     j check_loop               # العودة إلى الحلقة
 go_Next_EQ:
     addi $t1,$t1,1
-
-ADD_Results:
+ADD_Results:    
 add_results_loop:
     lb $t3, 0($t1)             # قراءة البايت الحالي
-    # إذا كانت القيمة صفرًا (نهاية النص)، انتقل إلى go_Next
-    beq $t3, $t6, go_Next_EQ      # إذا كانت '=', انتقل إلى go_Nex t
-    beqz $t3, go_Next
+    li $t9,45
+    beq $t3, $t9, negativeCoef# إذا كان الحرف '-'
+    
     li $t4, 13                 # ASCII لـ \r
-    beq $t3, $t4, skip_store   # إذا كانت القيمة \r، 
-    sb $t3, 0($t7)             # تخزين القيمة في Results
-    addi $t7, $t7, 1           # تحديث مؤشر Results
+    beq $t3, $t4, store_value  # إذا كانت القيمة \r، انتقل إلى store_value
+    beqz $t3, go_Next          # إذا كانت القيمة صفرًا (نهاية النص)، انتقل إلى go_Next
+    li $t6, 61                 # علامة '='
+    beq $t3, $t6, go_Next_EQ   # إذا كانت '='، انتقل إلى go_Next_EQ
+    subi $t3, $t3, 48          # تحويل الحرف الرقمي إلى رقم
+    mul $t5, $t5, 10           # ضرب الرقم السابق بـ 10
+    add $t5, $t5, $t3          # إضافة الرقم الجديد
     addi $t1, $t1, 1           # الانتقال إلى العنوان التالي
-    lb $t3, 0($t1)             # قراءة البايت التالي
-    li $v0, 11                 # syscall لطباعة حرف
-    move $a0, $t3              # تحميل الرقم للطباعة
-    syscall
+    j add_results_loop         # العودة للتحقق من البايت التالي
+negativeCoef:
+    move $s0,$t3
+    addi $t1, $t1, 1      # الانتقال إلى العنوان التالي
+    lb $t3, 0($t1)        # قراءة البايت التالي
+    j add_results_loop 
+store_value:
+    beq $s0,45,StoreNeg
+    sw $t5, 0($t7)             # تخزين القيمة في Results
+    addi $t7, $t7, 4           # تحديث مؤشر Results
+    li $t5, 0                  # إعادة تعيين المؤقت
+    addi $t1, $t1, 1           # الانتقال إلى العنوان التالي
+    li $s0,0
+    j add_results_loop         # العودة للتحقق من البايت التالي
+StoreNeg:
+    li $s0,0
+    negu $t5, $t5          # تحويل الرقم إلى قيمة سالبة
+    sw $t5, 0($t7)             # تخزين القيمة في Results
+    addi $t7, $t7, 4           # تحديث مؤشر Results
+    li $t5, 0                  # إعادة تعيين المؤقت
+    addi $t1, $t1, 1           # الانتقال إلى العنوان التالي
     j add_results_loop         # العودة للتحقق من البايت التالي
 
-skip_store:
-    addi $t1, $t1, 1           # الانتقال إلى العنوان التالي
-    lb $t3, 0($t1)             # قراءة البايت التالي
-    li $v0, 11                 # syscall لطباعة حرف
-    move $a0, $t3              # تحميل الرقم للطباعة
-    syscall
-    j add_results_loop         # العودة للتح   
- 
 next_char:
    # sw $t4, 0($t0)           # تخزين الرقم في Coefficient
    # addi $t0, $t0, 4         # الانتقال إلى الموقع التالي في المصفوفة
@@ -136,6 +165,24 @@ not_char:
     j check_loop          # العودة للتحقق من البايت التال
 
 is_char:
+    beq $s0,45,StoreNegCoeff
+    sw $t5, 0($t0)           # تخزين الرقم في Coefficient
+    addi $t0, $t0, 4         # الانتقال إلى الموقع التالي في المصفوفة
+    li $t5,0
+    la $t8 ,Variables
+    lb $t9,0($t8)
+    beq $t3,$t9,checkLoop
+    sb $t3,0($t2)
+    addi $t2,$t2,32
+    li $v0, 11            # syscall لطباعة حرف
+    move $a0, $t3         # تحميل الرقم إلى $a0
+    syscall
+    addi $t1, $t1, 1      # الانتقال إلى العنوان التالي
+    lb $t3, 0($t1)        # قراءة البايت التالي
+    j check_loop 
+StoreNegCoeff:
+    negu $t5, $t5 
+    li $s0,0
     sw $t5, 0($t0)           # تخزين الرقم في Coefficient
     addi $t0, $t0, 4         # الانتقال إلى الموقع التالي في المصفوفة
     li $t5,0
@@ -149,22 +196,111 @@ is_char:
     addi $t1, $t1, 1      # الانتقال إلى العنوان التالي
     lb $t3, 0($t1)        # قراءة البايت التالي
     j check_loop 
-    
+   
 checkLoop:
     addi $t8,$t8,32
     addi $t1,$t1,1
     lb $t3, 0($t1) 
     j check_loop 
     
+CR:
+  # --- حساب D ---
+    la $t1, Coefficient
+    subi $t1, $t1, 2
+    lw $t4, 0($t1)         # t4 = Coefficient[0]
+    addi $t1, $t1, 32
+    lw $t5, 0($t1)         # t5 = Coefficient[1]
     
+    la $t1, Coefficient
+    subi $t1, $t1, 2
+    addi $t1, $t1, 4
+    lw $t6, 0($t1)         # t6 = Coefficient[2]
+    addi $t1, $t1, 32
+    lw $t7, 0($t1)         # t7 = Coefficient[3]
+    
+    mul $t2, $t4, $t7      # t2 = t4 * t7
+    mul $t3, $t5, $t6      # t3 = t5 * t6
+    sub $t7, $t2, $t3      # t7 = D
+
+    # --- حساب Dx ---
+    la $t1, Results
+    subi $t1, $t1, 2
+    lw $t4, 0($t1)         # t4 = Results[0]
+    addi $t1, $t1, 32
+    lw $t5, 0($t1)         # t5 = Results[1]
+    
+    la $t1, Coefficient
+    subi $t1, $t1, 2
+    addi $t1, $t1, 4
+    lw $t6, 0($t1)         # t6 = Coefficient[2]
+    addi $t1, $t1, 32
+    lw $t7, 0($t1)         # t7 = Coefficient[3]
+    
+    mul $t2, $t4, $t7      # t2 = t4 * t7
+    mul $t3, $t5, $t6      # t3 = t5 * t6
+    sub $t8, $t2, $t3      # t8 = Dx
+
+    # --- حساب Dy ---
+    la $t1, Results
+    subi $t1, $t1, 2
+    lw $t6, 0($t1)         # t6 = Results[0]
+    addi $t1, $t1, 32
+    lw $t7, 0($t1)         # t7 = Results[1]
+    
+    la $t1, Coefficient
+    subi $t1, $t1, 2
+    lw $t4, 0($t1)         # t4 = Coefficient[0]
+    addi $t1, $t1, 32
+    lw $t5, 0($t1)         # t5 = Coefficient[1]
+    
+    mul $t2, $t4, $t7      # t2 = t4 * t7
+    mul $t3, $t5, $t6      # t3 = t5 * t6
+    sub $t9, $t2, $t3      # t9 = Dy
+
+    # --- تحويل النتائج إلى نقاط عائمة ---
+    mtc1 $t8, $f4          # نقل Dx إلى السجل العائم $f4
+    cvt.s.w $f4, $f4       # تحويل Dx إلى نقطة عائمة
+
+    mtc1 $t7, $f5          # نقل D إلى السجل العائم $f5
+    cvt.s.w $f5, $f5       # تحويل D إلى نقطة عائمة
+
+    mtc1 $t9, $f6          # نقل Dy إلى السجل العائم $f6
+    cvt.s.w $f6, $f6       # تحويل Dy إلى نقطة عائمة
+
+    # --- إجراء القسمة للحصول على X و Y ---
+    div.s $f0, $f4, $f5    # $f0 = Dx / D
+    div.s $f1, $f6, $f5    # $f1 = Dy / D
+
+    # --- تخزين النتائج في الذاكرة ---
+    s.s $f0, result1       # تخزين X في result1
+    s.s $f1, result2       # تخزين Y في result2
+
+    # --- طباعة النتائج ---
+    li $v0, 4
+    la $a0, newline
+    syscall
+
+    # طباعة X
+    l.s $f12, result1
+    li $v0, 2
+    syscall
+
+    li $v0, 4
+    la $a0, newline
+    syscall
+
+    # طباعة Y
+    l.s $f12, result2
+    li $v0, 2
+    syscall
+CR3:
 end_check:
     # طباعة سطر جديد بعد الأرقام
     li $v0, 4
     la $a0, newline
     syscall
     move $s7,$s6
-    #ble $t3, $s5, is_char    # إذا كانت بين 'A' و 'Z'، هي حرف
-    
+  
     # إنهاء البرنامج
     li $v0, 10            # syscall لإنهاء البرنامج
     syscall
@@ -197,12 +333,14 @@ copy_loop:
 end_copy:
     # تحريك المؤشر إلى المصفوفة التالية
     add $t9, $t9, $t8    # الانتقال إلى المصفوفة التالية في Arrays
-
+    lw $t5,LineCount
+    addi $t5,$t5,1
+    sw $t5,LineCount
     # إعادة تعيين المؤشر للمعادلة التالية
     la $t1, lineBuffer   # إعادة المؤشر إلى بداية lineBuffer
     addi $t0, $t0, 1     # تخطي محرف السطر الجديد
+    
     j read_line          # العودة لقراءة السطر التالي
-
 close_file:
     # إغلاق الملف
     li $v0, 16           # syscall لإغلاق الملف
